4b135f35fca6bf59e9dedac02030461afbcc2303
Uvecrep <Ian@peterson-family.com>
2019-09-18 17:43:11 -0700

    Actual shell script

---
+++ lab3_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Authors : Ian Peterson
+# Date: 9/18/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+echo "Please provide a file name:"
+read fileName
+echo "Please provide a regular expression for phone numbers:"
+read regEx1
+grep -c $regEx1 $fileName
+echo "Please provide a regular expression for phone numbers with area code 303:"
+read regEx2
+grep -o $regEx2 $fileName
+echo "Please provide a regular expression for emails:"
+read regEx3
+grep -o $regEx3 $fileName >> emails.txt
+
+#grep -c '\(([0-9]\{3\})\|[0-9]\{3\}\)[-]\+[0-9]\{3\}[-]\+[0-9]\{4\}' regex_practice.txt
+#or
+#egrep -c '[0-9]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
+
+#grep -c '.*\@.*\.*' regex_practice.txt
+
+#grep -o '^303.\{8\}' regex_practice.txt
+
+#grep -o '^.*@geocities.com' regex_practice.txt >> emails.txt 
